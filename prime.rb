0ddd2eab5c25eb50dc8d9bcce28d0348a5fd5949
SuperPox <calebkohl@gmail.com>
2021-01-18 20:44:56 -0700

    commit tree a629702bb6457ace3200386039d238c5735ed564

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+#return boolean if integer is prime
+#How do you create a range of numbers? How do you turn a range into an array so that it can be iterated over?
+def prime?(number)
+    if number <= 1
+        return false
+    end
+    i=2
+    while  i*i <= number do
+        if number % i == 0
+            return false
+        end
+        i = i + 1
+    end
+    return true
+end
+
+
+
